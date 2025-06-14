29c1e24ef022c588d61fb74f67f36dcf71f87014
rushan009 <dahalrushan5@gmail.com>
2025-06-14 15:05:57 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let nav=document.getElementById('navigation')
+let menu=document.querySelector('.ham_icon')
+function active() {
+    console.log("hi");
+    nav.classList.toggle('active')
+    menu.classList.toggle('active')
+}
